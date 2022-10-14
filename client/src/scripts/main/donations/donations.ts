447ffbaf59c5b21c160e47eec92143d3183d9916
Viacheslav Rudenko <slavikrudenko@gmail.com>
2022-10-14 21:50:28 +0300

    code optimiztion

--- client/src/scripts/main/donations/donations.ts
+++ client/src/scripts/main/donations/donations.ts
@@ -13,7 +13,7 @@ export default class Donations {
       cardnumber?: number;
       fullname?: string;
       cvv?: number;
-      expdate?: number;
+      expdate?: string;
     };
 
     btnSubmit.addEventListener("click", (e) => {
@@ -22,11 +22,26 @@ export default class Donations {
       for (const [key, value] of formValue) {
         value !== "" && (formData = { [key]: value });
       }
-      const sumSuccess = this.getSumValidation(formData.sum);
-      const ccSuccess = this.getCardNumValidation(formData.cardnumber);
-      const nameSuccess = this.getFullNameValidation(formData.fullname);
-      const cvvSuccess = this.getCvvValidation(formData.cvv);
-      const dateSuccess = this.getExpiryDateValidation(formData.expdate);
+      const sumSuccess = this.getValidation(
+        validateSum(formData.sum),
+        "sum-err"
+      );
+      const ccSuccess = this.getValidation(
+        checkCreditCard(formData.cardnumber),
+        "cc-err"
+      );
+      const nameSuccess = this.getValidation(
+        validateFullName(formData.fullname),
+        "fullname-err"
+      );
+      const cvvSuccess = this.getValidation(
+        validateCvvCode(formData.cvv),
+        "cvv-err"
+      );
+      const dateSuccess = this.getValidation(
+        validateExpiryDate(formData.expdate),
+        "date-err"
+      );
       if (sumSuccess && ccSuccess && nameSuccess && cvvSuccess && dateSuccess) {
         form.reset();
         console.log(formData);
@@ -43,35 +58,38 @@ export default class Donations {
       err.innerHTML = "";
     }
   }
-  getSumValidation(sum: number): boolean {
-    const result: Result = validateSum(sum);
-    const err = document.getElementById("sum-err");
-    this.addInputError(err, result);
-    return result.success;
-  }
+  // getSumValidation(sum: number): boolean {
+  //   const result: Result = validateSum(sum);
+  //   const err = document.getElementById("sum-err");
+  //   this.addInputError(err, result);
+  //   return result.success;
+  // }
 
-  getCardNumValidation(cardnumber: number): boolean {
-    const result: Result = checkCreditCard(cardnumber);
-    const err = document.getElementById("cc-err");
-    this.addInputError(err, result);
-    return result.success;
-  }
+  // getCardNumValidation(cardnumber: number): boolean {
+  //   const result: Result = checkCreditCard(cardnumber);
+  //   const err = document.getElementById("cc-err");
+  //   this.addInputError(err, result);
+  //   return result.success;
+  // }
 
-  getFullNameValidation(fullname: string): boolean {
-    const result: Result = validateFullName(fullname);
-    const err = document.getElementById("fullname-err");
-    this.addInputError(err, result);
-    return result.success;
-  }
-  getCvvValidation(cvv: number): boolean {
-    const result: Result = validateCvvCode(cvv);
-    const err = document.getElementById("cvv-err");
-    this.addInputError(err, result);
-    return result.success;
-  }
-  getExpiryDateValidation(expdate: string): boolean {
-    const result: Result = validateExpiryDate(expdate);
-    const err = document.getElementById("date-err");
+  // getFullNameValidation(fullname: string): boolean {
+  //   const result: Result = validateFullName(fullname);
+  //   const err = document.getElementById("fullname-err");
+  //   this.addInputError(err, result);
+  //   return result.success;
+  // }
+  // getCvvValidation(cvv: number): boolean {
+  //   const result: Result = validateCvvCode(cvv);
+  //   const err = document.getElementById("cvv-err");
+  //   this.addInputError(err, result);
+  //   return result.success;
+  // }
+  // getExpiryDateValidation(expdate: string): boolean {
+  //   this.getValidation(validateExpiryDate(expdate), "date-err");
+  // }
+  getValidation(validation: any, errId: string): boolean {
+    const result: Result = validation;
+    const err = document.getElementById(errId);
     this.addInputError(err, result);
     return result.success;
   }
