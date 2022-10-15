5b25394afc2898ea768376723fd68057b196caa2
Viacheslav Rudenko <slavikrudenko@gmail.com>
2022-10-15 10:13:15 +0300

    change data of what we need

--- client/src/scripts/main/we-need/data.ts
+++ client/src/scripts/main/we-need/data.ts
@@ -50,7 +50,7 @@ export const productListData: DataArray = [
       "https://d.428.ua/img/3414963/600/600/kevlarovyy_shlem_s_zakrytymi_ushami_zelenyy_19091~744~934.jpg",
     name: "Мед рюкзаки 20-30л",
     collected: 900,
-    purchased: 300,
+    purchased: 700,
     remains: 200,
   },
 ];
