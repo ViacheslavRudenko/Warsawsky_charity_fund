912b2e82e27b960df52e499db87c87fb31958a47
Viacheslav Rudenko <slavikrudenko@gmail.com>
2022-10-15 10:12:07 +0300

    delete unuseful dependency

--- client/src/scripts/main/we-need/data.ts
+++ client/src/scripts/main/we-need/data.ts
@@ -17,7 +17,7 @@ export const productListData: DataArray = [
   {
     imgUrl: "https://images.prom.ua/3979331917_taktichna-vijskova-aptechka.jpg",
     name: "Мед рюкзаки 20-30л",
-    collected: 900,
+    collected: 500,
     purchased: 300,
     remains: 200,
   },
@@ -25,24 +25,24 @@ export const productListData: DataArray = [
     imgUrl:
       "https://d.428.ua/img/3414963/600/600/kevlarovyy_shlem_s_zakrytymi_ushami_zelenyy_19091~744~934.jpg",
     name: "Мед рюкзаки 20-30л",
-    collected: 900,
-    purchased: 300,
+    collected: 9400,
+    purchased: 9230,
     remains: 200,
   },
   {
     imgUrl:
       "https://d.428.ua/img/3414963/600/600/kevlarovyy_shlem_s_zakrytymi_ushami_zelenyy_19091~744~934.jpg",
     name: "Мед рюкзаки 20-30л",
-    collected: 900,
-    purchased: 300,
+    collected: 2900,
+    purchased: 2300,
     remains: 200,
   },
   {
     imgUrl:
       "https://d.428.ua/img/3414963/600/600/kevlarovyy_shlem_s_zakrytymi_ushami_zelenyy_19091~744~934.jpg",
     name: "Мед рюкзаки 20-30л",
-    collected: 900,
-    purchased: 300,
+    collected: 4900,
+    purchased: 5300,
     remains: 200,
   },
   {
